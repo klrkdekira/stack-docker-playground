f04c6632f92642559f4b365e0d96ef7dcf917933
Chee Leong <klrkdekira@gmail.com>
2017-09-22 22:08:24 +0800

    First commit

---
+++ src/Main.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Control.Applicative
+import Control.Monad (unless)
+import Options
+
+data MainOptions = MainOptions
+  { optFile :: String }
+
+instance Options MainOptions where
+  defineOptions = pure MainOptions
+    <*> simpleOption "file" "" "File to read"
+
+main :: IO ()
+main = runCommand $ \opts args -> do
+  let file = optFile opts in
+    if file == ""
+       then putStrLn file
+       else putStrLn "failed"
